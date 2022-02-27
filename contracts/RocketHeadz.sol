4a01328715fe36362c16f830cafcb02cb9038091
Sneh1999 <snehkoul1999@gmail.com>
2022-02-27 18:02:20 -0500

    add erc721a

---
+++ contracts/RocketHeadz.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.4;
+
+import "erc721a/contracts/ERC721A.sol";
+
+contract RocketHeadz is ERC721A {
+    using Strings for uint256;
+
+    constructor() ERC721A("RocketHeadz", "RH") {}
+
+    function mint(uint256 quantity) external payable {
+        // _safeMint's second argument now takes in a quantity, not a tokenId.
+        _safeMint(msg.sender, quantity);
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        string memory baseURI = _baseURI();
+        return
+            bytes(baseURI).length != 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString()))
+                : "";
+    }
+}
