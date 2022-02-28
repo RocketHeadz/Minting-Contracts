747fa96fa7be4ba3797235450a377064b8b01595
Sneh1999 <snehkoul1999@gmail.com>
2022-02-27 19:39:55 -0500

    Complete the implementation for ERC721-A

--- contracts/RocketHeadz.sol
+++ contracts/RocketHeadz.sol
@@ -1,33 +1,70 @@
 pragma solidity ^0.8.4;
 
 import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RocketHeadz is ERC721A {
+// ______           _        _   _   _                _
+// | ___ \         | |      | | | | | |              | |
+// | |_/ /___   ___| | _____| |_| |_| | ___  __ _  __| |____
+// |    // _ \ / __| |/ / _ \ __|  _  |/ _ \/ _` |/ _` |_  /
+// | |\ \ (_) | (__|   <  __/ |_| | | |  __/ (_| | (_| |/ /
+// \_| \_\___/ \___|_|\_\___|\__\_| |_/\___|\__,_|\__,_/___|
+
+contract RocketHeadz is ERC721A, Ownable {
     using Strings for uint256;
 
-    constructor() ERC721A("RocketHeadz", "RH") {}
+    string public baseURI;
 
-    function mint(uint256 quantity) external payable {
-        // _safeMint's second argument now takes in a quantity, not a tokenId.
-        _safeMint(msg.sender, quantity);
+    /**** CONSTANTS ****/
+    uint256 public constant MINT_PRICE = 0.08 ether;
+    uint128 public constant MAX_MINTS_PER_TX = 10;
+    uint128 public constant MAX_ROCKETHEADZ = 11111;
+
+    constructor(string memory _baseURI) ERC721A("RocketHeadz", "RH") {
+        baseURI = _baseURI;
     }
 
     /**
-     * @dev See {IERC721Metadata-tokenURI}.
+     * Returns the number of tokens minted by the owner
+     */
+    function numberMinted(address owner) public view returns (uint256) {
+        return _numberMinted(owner);
+    }
+
+    /**
+     * @dev
+     * This implementation returns the tokenURI
+     * based on the tokenId
      */
     function tokenURI(uint256 tokenId)
         public
         view
-        virtual
         override
         returns (string memory)
     {
         if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
-
-        string memory baseURI = _baseURI();
         return
             bytes(baseURI).length != 0
                 ? string(abi.encodePacked(baseURI, tokenId.toString()))
                 : "";
+    }
+
+    /**
+     * @dev Mints `quantity` tokens and transfers them to `to`.
+     */
+    function mint(
+        address to,
+        uint256 quantity,
+        bytes memory _data,
+        bool safe
+    ) public payable {
+        require(quantity <= MAX_MINTS_PER_TX, "MAX_MINT_PER_TX_EXCEDDED");
+        uint256 supply = totalSupply();
+        require(supply + quantity <= MAX_ROCKETHEADZ, "MAX_SUPPLY_EXCEEDED");
+        require(
+            msg.value >= MINT_PRICE * quantity,
+            "INSUFFICIENT_ETH_FOR_MINT"
+        );
+        _mint(to, quantity, _data, safe);
     }
 }
